76f528ee2422278a7151eb6ba13f74c9715b514b
ahnaf-asif <ahnafshahriar92@gmail.com>
2024-07-14 18:16:19 +0600

    add mantine theme

---
+++ src/Config/MantineTheme.ts
@@ -0,0 +1,8 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+const customThemeConfig: MantineThemeOverride = {
+  fontFamily: 'inter ,sans-serif',
+  defaultRadius: '3px'
+};
+
+export default customThemeConfig;
